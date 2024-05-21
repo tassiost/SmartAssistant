45958253004a116fe1ac14cd8e8cf99a5ca91445
Tassio Steinmann <57908759+tassiost@users.noreply.github.com>
2024-05-21 15:53:12 +0200

    Update llm.py

--- llm.py
+++ llm.py
@@ -25,7 +25,7 @@ HOST = f'localhost:{PORT}'          #HOST = 'localhost:5005'
 
 # For reverse-proxied streaming, the remote will likely host with ssl - wss://
 # URI = 'wss://your-uri-here.trycloudflare.com/api/v1/stream'
-URIprefixValue = "defence-panama-center-demo"
+URIprefixValue = "remote-honors-desert-rg"
 
 st.set_page_config(
     page_title="Chat",
