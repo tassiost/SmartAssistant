9fda6fdcdc7d95248ad78fe24348e97a57bcd00d
Tassio Steinmann <57908759+tassiost@users.noreply.github.com>
2024-05-24 17:58:38 +0200

    Update llm.py

--- llm.py
+++ llm.py
@@ -25,7 +25,7 @@ HOST = f'localhost:{PORT}'          #HOST = 'localhost:5005'
 
 # For reverse-proxied streaming, the remote will likely host with ssl - wss://
 # URI = 'wss://your-uri-here.trycloudflare.com/api/v1/stream'
-URIprefixValue = "established-phases-comedy-natural"
+URIprefixValue = "infrared-disable-interviews-dublin"
 
 st.set_page_config(
     page_title="Chat",
